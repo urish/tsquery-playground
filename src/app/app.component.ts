4c12c574a0d54b32df3d06b1e06c83ea6267d3b3
kevin schuchard <schuchard.kevin@gmail.com>
2019-09-20 21:09:30 -0600

    styles - support mobile stacking - make AST scroll X,Y

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ const matchHighlightClass = 'ast-match-highlight';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements AfterViewInit {
   @ViewChild('codeEditor', { static: false }) codeEditor: CodemirrorComponent;
